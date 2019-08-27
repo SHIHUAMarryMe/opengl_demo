7c5a7e3c44f877972746f1f86caab1a7c050a5a7
shihua <878694891@qq.com>
2019-08-27 22:49:42 +0800

    feat: glfwSetScrollCallbac->glfwSetScrollCallback and grab mouse.

--- mesh/mesh/main.cpp
+++ mesh/mesh/main.cpp
@@ -175,10 +175,10 @@ int main()
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSetCursorPosCallback(window, mouse_callback);
-	glfwSetScrollCallbac(window, scroll_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
 	// tell GLFW to capture our mouse
-	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// glad: load all OpenGL function pointers
 	// ---------------------------------------
