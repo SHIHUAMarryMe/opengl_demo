8bb4026a01f6ca701215e039d528c5b5a64291cf
shihua <878694891@qq.com>
2019-09-04 21:19:16 +0800

    feat: grayscaling.

--- framebuffer_primary/framebuffer_primary/glsl/quad_framebuffer_fragment_shader.glsl
+++ framebuffer_primary/framebuffer_primary/glsl/quad_framebuffer_fragment_shader.glsl
@@ -7,6 +7,7 @@ uniform sampler2D screen_texture;
 
 void main()
 {
-    vec3 rgb_value = texture(screen_texture, TexCoords).rgb;
-    frag_color = vec4(vec3(1.0-rgb_value), 1.0);
+    frag_color = texture(screen_texture, TexCoords);
+    float average_value = (frag_color.r + frag_color.g + frag_color.b)/3.0f;
+    frag_color = vec4(average_value, average_value, average_value, 1.0f);
 }
